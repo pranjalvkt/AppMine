995c3a4fcf6af8987b8e48ba2c0d0ac76fe2c96b
Pranjal Tripathi <64363696+pranjalvkt@users.noreply.github.com>
2022-04-13 17:19:08 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const User = require('./Models/userModel');
+const Admin = require('./Models/adminModel');
 const path = require('path');
 const app = express();
 const bodyParser = require("body-parser");
 const fs = require('fs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-
 const db = 'mongodb+srv://admin123:PranjalT%400809@cluster1.acwvi.mongodb.net/appmine';
-// ?retryWrites=true&w=majority
 
 mongoose.connect(db, {
     useNewUrlParser : true,
@@ -26,13 +25,11 @@ const appSchema = new mongoose.Schema({
     image: String, 
     title: String,
     para: String,
-    name: String
+    name: String,
+    appAdmin: String,
+    appOS: String
 });
-
 const appsData = mongoose.model("app", appSchema);
-
-
-
 
 
 app.get('/', (req, res) => {
@@ -49,7 +46,8 @@ app.get('/success', (req, res) => {
 
 // HOME PAGE FOR WINDOWS
 app.get('/windowsHome', (req, res)=>{
-    appsData.find(function(err, app) {
+    var query = { appOS: "windows" };
+    appsData.find(query, function(err, app) {
         if(err) {
            console.log(err);
         } else {
@@ -57,7 +55,6 @@ app.get('/windowsHome', (req, res)=>{
         }
     })
 });
-
 
 // **************************User Part***********************************//
 
@@ -73,23 +70,61 @@ app.get('/userSignUp', (req, res) => {
 
 //user signin details
 app.post('/userSignIn', (req, res) => {
+    
+    const currEmail = req.body.userSignin_email;
+    const currPassword = req.body.userSignin_password;
+    
+    User.findOne({email: currEmail}, (err, foundUser)=>{
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                if(foundUser.password === currPassword) {
+                    res.redirect('/home');
+                } else {
+                    console.log("Invalid email/password");
+                    res.redirect('/userSignIn')
+                }
+            }
+        }
+    })
 
-    let userSignin = {
-        email : req.body.userSignin_email,
-        password: req.body.userSignin_password,
-    }
-    console.log(userSignin);
 });
 
 //user signup details
 app.post('/userSignUp', (req, res) => {
-
-    let userSignup = {
+ 
+    const currEmail = req.body.userSignup_email;
+    const currPassword = req.body.userSignup_password;
+    const newUser = new User ({
         name: req.body.userSignup_name,
-        email : req.body.userSignup_email,
-        password: req.body.userSignup_password,
-    }
-    console.log(userSignup);
+        email : currEmail,
+        password: currPassword,
+    })
+    User.findOne({email: currEmail}, (err, foundUser)=>{
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                if(foundUser.password === currPassword) {
+                    console.log("User already exist");
+                    res.redirect('/home');
+                } else {
+                    res.redirect('/userSignUp')
+                }
+            } else {
+                newUser.save((err) => {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log("New user created");
+                        res.redirect('/home')
+                    }
+                })
+            }
+        }
+    })
+    
 });
 
 //List of Apps
@@ -114,10 +149,9 @@ app.get('/home', (req, res) => {
 //Individual app page
 app.get("/appPages/:value", (req, res) => {
     value = req.url;
-    // console.log(value);
+    // console.log();
     res.sendFile(__dirname+value);
 });
-
 
 app.get('/download/:value',function(req,res){
     value = req.url;
@@ -162,14 +196,57 @@ app.get('/adminHome', (req, res) => {
     res.sendFile(__dirname + '/adminHome.html')
 });
 
+app.get('/deleteApp', (req, res)=> {
+    // DELETE APP CREATED BY CURRENT ADMIN
+    let value = req.url;
+    let toRemove = value.slice(10, -5);
+    let query = { name: 'dfd' };
+    appsData.deleteOne(query, function(err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        // console.log(obj);
+        res.sendFile(__dirname + '/deleted.html');
+    });
+})
+
+app.get('/createdApp', (req, res)=> {
+    // APP CREATED BY CURRENT ADMIN
+    let currAdmin = 'pranjal123';
+
+    let query = { appAdmin: currAdmin };
+    appsData.find(query, function(err, app) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("currAdminAppAll.ejs", {app: app});
+        }
+        
+    });
+})
+
 app.get('/publishApp', (req, res) => {
+    // PAGE FOR CREATING NEW APP
     res.sendFile(__dirname + '/PublishApp.html')
 });
 
 app.post('/publishApp', (req, res)=>{
-
+    // PAGE FOR CREATING NEW APP
     let appName = req.body.appName;
     let appDesc = req.body.appDesc;
+    let appImg = req.body.appImg;
+    let appAdmin = req.body.appAdmin;
+    let appOS = req.body.appOS;
+    // let appOS = tempAppOS.toLowerCase()
+    const mong = new appsData ({
+        name: appName.toLowerCase(),
+        image: appImg,
+        para: appDesc,
+        title: appName,
+        appAdmin: appAdmin,
+        appOS: appOS
+    });
+    
+    appName = appName.toLowerCase();
     let contentToCopy = `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -206,6 +283,14 @@ app.post('/publishApp', (req, res)=>{
     fs.appendFile('appPages/' + appName + '.html', contentToCopy, function (err) {
         if (err) throw err;
         console.log('Saved!');
+    });
+    appsData.insertMany([mong], function(err){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("New app detail successfully added !");
+            res.redirect('/success');
+        }
     });
 })
 
